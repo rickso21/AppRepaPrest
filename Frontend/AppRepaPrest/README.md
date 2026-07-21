97e530e2cdf371d2faf9d677cf627243b7061967
Luis Eduardo Ruiz Ignacio <rickso21111@gmail.com>
2026-07-20 20:07:24 -0600

    cambios el 20 de julio

---
+++ Frontend/AppRepaPrest/README.md
@@ -0,0 +1,2 @@
+# AppRepaPrest
+# AppRepaPrest
---
+++ Frontend/AppRepaPrest/src/navigation/AppNavigator.tsx
@@ -0,0 +1,54 @@
+// src/navigation/AppNavigator.tsx
+import React, { JSX } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// IMPORTANTE: Verifica que estas rutas sean correctas
+import Welcome from '../screens/Welcome/Welcome';
+import Login from '../screens/Login/Login';
+import Home from '../screens/Home/Home';
+
+export type RootStackParamList = {
+  Welcome: undefined;
+  Login: undefined;
+  Home: {
+    userId?: number;
+    userName?: string;
+    userEmail?: string;
+    userPhone?: string;
+  } | undefined;
+
+  };
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+export default function AppNavigator(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Welcome"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen 
+          name="Login" 
+          component={Login}
+          options={{
+            animationTypeForReplace: 'push',
+            animation: 'slide_from_right',
+          }}
+        />
+        <Stack.Screen 
+          name="Home" 
+          component={Home}
+          options={{
+            animationTypeForReplace: 'push',
+            animation: 'slide_from_right',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
